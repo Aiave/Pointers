34636d8b2ca10d2d668d4898b5aea1dcdbb0a204
Aiave <kiterin.kato@gmail.com>
2023-08-19 03:45:21 +0300

    Добавлено меню выбора заданий.

--- Pointers/main.c
+++ Pointers/main.c
@@ -6,8 +6,36 @@ int main()
 {
     setlocale(LC_ALL, "Russian");
 
-    guide();
-    pointers();
+    int task;
+    do
+    {
+        printf("1. Указатели на простом примере. (+ гайд в комментарии)\n"
+        "2. Экспериментируем со * и &, смотрим, что будет в консоли.\n"
+        "3. Примеры с функциями (передача по значению, передача по указателю).\n"
+        "4. \n"
+        "5. \n\n"
+        "Что будем делать? (1-5)\n\n");
+        scanf_s("%d", &task);
+    } while (task < 0 || task > 5);
+
+    switch (task)
+    {
+    case 1:
+        guide();
+        break;
+    case 2:
+        pointers();
+        break;
+    case 3:
+        guide_functions();
+        break;
+    case 4:
+        break;
+    case 5:
+        break;
+    default:
+        break;
+    }
 
     return 0;
 }
