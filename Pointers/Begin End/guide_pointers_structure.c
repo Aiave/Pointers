718e2802151ba903510d851cf6516b7222cf1437
Aiave <kiterin.kato@gmail.com>
2023-08-22 04:17:32 +0300

    Добавлены примеры передачи структуры в функцию. Добавлен заголовочный файл.

--- Pointers/Begin End/guide_pointers_structure.c
+++ Pointers/Begin End/guide_pointers_structure.c
@@ -1,14 +1,7 @@
 ﻿#include <stdio.h>
+#include "guide_pointers_structure.h"
 
 //Спасибо Begin End!
-
-struct coordinates
-{
-	int x;
-	int y;
-};
-
-typedef struct coordinates coordinate;
 
 void guide_structure()
 {
@@ -28,6 +21,24 @@ void guide_structure()
 		(*pointer).x = 22;
 		(*pointer).y = 55;
 		printf("x = %d\ny = %d\n", coord.x, coord.y);
+	}
+
+	print(*pointer);
+	print_by_pointer(pointer, 11, 12);
+}
+
+void print(coordinate point)
+{
+	printf("\nx = %d\ny = %d\n", point.x, point.y);
+}
+
+void print_by_pointer(coordinate* pointer, int x_position, int y_position)
+{
+	if (pointer != NULL)
+	{
+		(*pointer).x = x_position;
+		(*pointer).y = y_position;
+		printf("\nx = %d\ny = %d\n", x_position, y_position);
 	}
 }
 
@@ -56,4 +67,26 @@ printf("x = %d\ny = %d\n", coord.x, coord.y);
 В примере:
 1. Обратились по адресу указателя, получили доступ к переменной coord, которая является структурой. (*pointer)
 2. После этого обращаемся к переменным внутри структуры и меняем их значения. ((*pointer).x, (*pointer).y)
+
+Передавать структуры в функции и процедуры можно также, как обычные переменные.
+
+Передача по значению (копируется из coord в point при передаче в функцию):
+void print(coordinate point)
+{
+	printf("\nx = %d\ny = %d\n", point.x, point.y);
+}
+
+Передача по указателю используется, когда нужно изменить значение самой переменной coord.
+Указатель *pointer уже содержит адрес переменной coord, поэтому на вход можно передать его.
+Было:
+print_by_pointer(&coord, 11, 12);
+Стало:
+print_by_pointer(pointer, 11, 12);
+
+При вызове процедуры print можно тоже использовать указатель.
+Было:
+print(coord);
+Стало:
+print(*pointer);
+Работает так: сначала переходим по адресу указателя, получаем значение переменной coord, которое и было передано до этого.
 */
---
+++ Pointers/Begin End/guide_pointers_structure.h
@@ -0,0 +1,15 @@
+﻿#ifndef GUIDE_POINTERS_STRUCTURE
+#define GUIDE_POINTERS_STRUCTURE
+
+struct coordinates
+{
+	int x;
+	int y;
+};
+
+typedef struct coordinates coordinate;
+
+void print(coordinate point);
+void print_by_pointer(coordinate *pointer, int x_position, int y_position);
+
+#endif
